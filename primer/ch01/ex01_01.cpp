8c447b23c7be0a6438e5d7dd65e4737a1313cec4
daebecodin <136534615+daebecodin@users.noreply.github.com>
2026-02-11 05:25:27 -0800

    exercises 1 and 2

---
+++ primer/ch01/ex01_01.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Durand Dyer-Branch on 2/11/26.
+//
+
+int main()
+{
+    return 0;
+}
---
+++ primer/ch01/hello.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Durand Dyer-Branch on 2/11/26.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, C++ Primer\n";
+    return 0;
+}
