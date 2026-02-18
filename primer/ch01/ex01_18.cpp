e4bd6f1c010f533d58ff4668a50dc0e245a20b4b
daebecodin <136534615+daebecodin@users.noreply.github.com>
2026-02-18 12:33:53 -0800

    addition examples

--- primer/ch01/ex01_18.cpp
+++ primer/ch01/ex01_18.cpp
@@ -15,7 +15,7 @@ int main()
         while (std::cin >> val) { // reads next input value and stores in val
             if (val == currVal) { // if same then
                 ++cnt; // increment count
-            } else { // if the match chain ends
+                } else { // if the match chain ends
                 std::cout << currVal << " occurs " << cnt << " times" << std::endl; // print out occurrence
                 currVal = val; // make the new current the next in the line of inputs
                 cnt = 1; //reset count
