63ad5fd72b93a686d319c31f7319cb9bf1ec1523
daebecodin <136534615+daebecodin@users.noreply.github.com>
2026-02-21 18:23:14 -0800

    mixing signed and unsigned

--- primer/ch02/experiments/mixing_signed_unsigned.cpp
+++ primer/ch02/experiments/mixing_signed_unsigned.cpp
@@ -3,6 +3,7 @@
  *
  * When signed and unsigned ints are used in the same expression, if the unsigned int is of equal or greater rank,
  * the signed operand is converted to unsigned.
+ *
  * Negative numbers are now converted module 2^n, n being the number of bits for the type
  *
  * This conversion is dangerous because negative signed values may be converted to large unsigned positive values,
