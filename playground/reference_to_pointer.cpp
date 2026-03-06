6081bbfcfbc9124a1eeedd7b84afd9970c4cbefb
daebecodin <duranddyer123@gmail.com>
2026-03-06 09:17:38 -0800

    experiment templates

---
+++ playground/reference_to_pointer.cpp
@@ -0,0 +1,36 @@
+/*
+ * Created by imdem on 3/6/2026.
+*/
+
+#include <iostream>
+#include "report.h"
+using namespace std;
+using namespace rpt;
+
+int main()
+{
+    header(
+      "",
+      ""
+    );
+
+    inputs({
+        "",
+        ""
+    });
+
+    pipeline_begin();
+
+    classify({
+        "",
+        "",
+        ""
+    });
+
+    rule({
+        "",
+        "",
+        ""
+    });
+    return 0;
+}
