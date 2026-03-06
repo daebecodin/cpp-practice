1e5215c780d1006150150e4400020ddc993995ca
daebecodin <duranddyer123@gmail.com>
2026-03-06 11:23:07 -0800

    report_template.cpp

---
+++ playground/report_template.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "report.h"
+using namespace std;
+using namespace rpt;
+
+int main()
+{
+    header(
+      "",
+      ""
+    );
+
+    inputs({
+        "",
+        ""
+    });
+
+    pipeline_begin();
+
+    classify({
+        "",
+        "",
+        ""
+    });
+
+    rule({
+        "",
+        "",
+        ""
+    });
+    return 0;
+}
