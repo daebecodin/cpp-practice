3159e5d405644c4d784223b7b39f4bbd02b93aed
daebecodin <duranddyer123@gmail.com>
2026-03-12 01:15:47 -0700

    const_reference_conversions.cpp

--- playground/const/const_reference_conversions.cpp
+++ playground/const/const_reference_conversions.cpp
@@ -17,10 +17,35 @@ int main()
 
     inputs({
         "No Inputs",
-        "Objects Involved: { int i, }"
+        "Objects Involved: { int i, const int ci, int &r, const int &rc }"
     });
 
     pipeline_begin();
+    int i = 420; // int
+    const int ci = 67; // const int
+    int &r = i; // reference to an int
+    const int &rc = i; // reference toa const int
+
+    kv("i -> ", i); // 420;
+    kv("ci -> ", ci); // 67
+    kv("r -> ", r); // 420
+    kv("rc -> ", rc); //420
+    kv("&i -> ", &i); // address of i
+    kv("&ci -> ", &ci); // address of ci
+    kv("&r -> ", &i); // address of i
+    kv("&rc -> ", &i); // address of i
+    blank();
+
+    stage("Comparisons");
+    kv("r == i -> ", r == i); // true
+    kv("rc == i -> ", rc == i); // true
+    kv("&r == &i -> ", &r == &i); // trur
+    kv("&rc == &i -> ", &rc == &i); // true
+    blank();
+
+    i = 69;
+    rc = ci;
+
 
     classify({
         "",
