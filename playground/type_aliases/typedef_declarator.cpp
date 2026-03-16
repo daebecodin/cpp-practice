267cc37fc817380a3f6208f9116dc8e73863b03d
daebecodin <duranddyer123@gmail.com>
2026-03-16 16:30:42 -0700

    typedef_declarator.cpp

---
+++ playground/type_aliases/typedef_declarator.cpp
@@ -0,0 +1,35 @@
+/*
+ * Created by imdem on 3/15/2026.
+*/
+#include <iostream>
+#include "report.h"
+using namespace std;
+using namespace rpt;
+
+int main()
+{
+    header(
+     "",
+     ""
+   );
+
+    inputs({
+        "",
+        ""
+    });
+
+    pipeline_begin();
+
+    classify({
+        "",
+        "",
+        ""
+    });
+
+    rule({
+        "",
+        "",
+        ""
+    });
+    return 0;
+}
