57cef3f5e1d3b25faccc3eb66c6cf4241ca9142d
daebecodin <136534615+daebecodin@users.noreply.github.com>
2026-03-29 17:08:55 -0700

    example 1 chapter 3

---
+++ primer/ch03/example1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using std::cout; using std::endl;
+
+int main() {
+int sum{}, val{1};
+
+while (val <= 10) {
+    sum += val;
+    ++val;
+}
+cout << "Sum of 1-10 inclusive -> " << sum << endl;
+return 0;
+}
+
+
