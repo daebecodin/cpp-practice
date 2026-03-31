7ae6743ade58bfb9b0782dcbf5a29ce6e8b37360
daebecodin <136534615+daebecodin@users.noreply.github.com>
2026-03-30 20:11:16 -0700

    vim restart

--- primer/ch03/string.cpp
+++ primer/ch03/string.cpp
@@ -2,9 +2,24 @@
 #include <iostream>
 using namespace std;
 using std::string;
+using std::cout, std::cin, std::endl, std::boolalpha;
 
 int main() 
 {
+    /*
+     * String Initialization
+     */
+    string s1; // empty string
+    string s2 = s1; // copy of s1
+    string s3 = "copy"; // copy of the string literal
+    string s4(6, 's'); // 6 counts of s 
+    string s5("copy");
+    string s6("copy");
+    string s7 = string(6,'d');
+
+
+    
+
 
     return 0;
 
