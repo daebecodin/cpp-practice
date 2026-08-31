21f2e55f86ee69e3283d77eaf10ec97390a51ba7
daebecodin <136534615+daebecodin@users.noreply.github.com>
2026-08-30 20:58:42 -0700

    idk

--- playground/vector/vector_pair_sums.cpp
+++ playground/vector/vector_pair_sums.cpp
@@ -1,1 +1,16 @@
+/*
+* Read a set of integers into a vector. Print the sum of each
+* pair of adjacent elements. Change your program so that it prints the sum
+* of the first and last elements, followed by the sum of the second and
+* second-to-last, and so on.
+*/
+
 #include "pch.h"
+
+int main() 
+{
+    int i;
+
+    return 0;
+
+} 
