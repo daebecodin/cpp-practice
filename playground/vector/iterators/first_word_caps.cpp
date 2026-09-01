ce56b309a93e9ff3aa33531ab0b98ce3021e7770
daebecodin <136534615+daebecodin@users.noreply.github.com>
2026-09-01 13:26:15 -0700

    finished first word cap solution using std::getlin

--- playground/vector/iterators/first_word_caps.cpp
+++ playground/vector/iterators/first_word_caps.cpp
@@ -5,15 +5,22 @@
 
 #include "pch.h"
 using std::string;
-using std::cin, std::cout;
+using std::cin, std::cout, std::getline;
+using std::vector;
+using std::size_t;
+using std::toupper;
 int main() 
 {
-    string text{};
-    while (cin >> text) 
-    {
-        
 
+    cout << "Enter a full scentence: ";
+    string text{};
+    getline(cin, text);
+    for (auto it = text.begin(); it != text.end() && *it != ' '; ++it)
+    {
+        *it = toupper(*it);
     }
+
+    cout << text;
     return 0;
 
 }
