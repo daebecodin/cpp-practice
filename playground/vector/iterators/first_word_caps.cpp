005fe69dce00ed92742c79954f607ac89ebb5088
daebecodin <136534615+daebecodin@users.noreply.github.com>
2026-09-01 12:55:52 -0700

    starting first word caps exercise

---
+++ playground/vector/iterators/first_word_caps.cpp
@@ -0,0 +1,19 @@
+/*
+ * 
+ */
+
+
+#include "pch.h"
+using std::string;
+using std::cin, std::cout;
+int main() 
+{
+    string text{};
+    while (cin >> text) 
+    {
+        
+
+    }
+    return 0;
+
+}
