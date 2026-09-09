f5a5d43ddccb8eb3ac88dc477641642f05d22ca1
daebecodin <136534615+daebecodin@users.noreply.github.com>
2026-09-09 12:40:07 -0700

    array to pointer

--- playground/arrays/array_to_pointer.cpp
+++ playground/arrays/array_to_pointer.cpp
@@ -12,28 +12,31 @@ int main()
     // create 1 array
     int arr1[] = {1, 2, 3, 4, 5};
 
-    // 2 raw pointers to array to first elements
+    // both pointers point to the arrays first element
     int* p1 = arr1;
     int* p2 = &arr1[0];
 
-    // modify an element from one access path
+    // modify the array through a pointer
     p2[2] = 67;
     cout << arr1[2] << '\n';
 
-    // print both arrays
+    // both pointers should contain the same address
     cout << p1 << '\n' << p2 << '\n';
 
 
-    // create a pointer using auto
+    // auto sees the array converted as a pointer
     auto a = arr1; // pointer to first element
 
-    // create type alias using decltype()
+    // decltype preserves the array type
     decltype(arr1) copy = {5, 6, 7, 8};
 
     // confirm type
     cout << boolalpha;
-    cout << std::is_pointer_v<decltype(arr1)> << '\n';
-    cout << std::is_array_v<decltype(copy)> << '\n';
+    cout << "Is a pointer?: " 
+         << std::is_pointer_v<decltype(a)> << '\n';
+
+    cout << "Is an array?: " 
+         << std::is_array_v<decltype(copy)> << '\n';
 
 
     return 0;
