4ff4f72693a2ff5ae43129b53d5aec2aafb259cd
daebecodin <136534615+daebecodin@users.noreply.github.com>
2026-09-09 18:10:12 -0700

    pointer arithmetic

---
+++ playground/arrays/pointer_arithmetic.cpp
@@ -0,0 +1,29 @@
+/*
+ * 
+ */
+
+#include "pch.h"
+using std::cout, std::string;
+
+int main() 
+{
+    constexpr int sz = 10;
+    int arr[sz] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    int* b = arr; // points to element[0]
+    int* e = b + 10; // points to one past last; handle for end
+    int* e2 = arr + 10; // use with caution; do not dereference
+
+    int i = 0;
+    while (b < e)
+    {
+        cout << *b ; //dereferencing the current position
+        ++b;
+
+    }
+
+
+    return 0;
+
+
+}
